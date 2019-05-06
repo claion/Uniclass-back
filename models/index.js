99621bd57f512267aef824c5aecebc824b595b35
changhoi0522 <0417kch@naver.com>
2019-05-07 01:09:11 +0900

    signup is done

---
+++ models/index.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+export default () => {
+    const connect = () => {
+        if (process.env.NODE_ENV !== 'production') {
+            mongoose.set('debug', true);
+        }
+
+        mongoose.connect(process.env.MONGO_URL, {
+            useFindAndModify: false,
+            useNewUrlParser: true
+        }, (error) => {
+            if (error) {
+                console.error('몽고디비 연결 에러', error);
+            } else {
+                console.log('몽고디비 연결 성공')
+            }
+        });
+    }
+    connect();
+
+    mongoose.connection.on('error', (e) => {
+        console.error('몽고디비 연결 에러', e);
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.error('몽고디비 연결 끊어짐, 재연결');
+        connect();
+    })
+
+    require('./user');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+import {signup} from '../controllers/userController'
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post('/signup', signup);
 
 module.exports = router;
