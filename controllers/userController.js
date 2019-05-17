5c3ac9d58ec3dbe85634e0f55200cebe72285c81
changhoi <0417kch@naver.com>
2019-05-17 19:50:53 +0900

    jwt토큰 인증 방식에 문제가 있어서 바꿨음 지금 잘 됨

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
-import http from 'http-status-codes'
+import http from 'http-status-codes';
 
 export const getProfile = (req, res, next) => {
-    res.status(http.ACCEPTED).json("success");
-}
+  res.status(http.ACCEPTED).json({ success: true, data: { user: req.user } });
+};
+
+export const verifyEmail = (req, res, next) => {};
--- models/emailToken.js
+++ models/emailToken.js
@@ -1,21 +1,26 @@
 import mongoose from 'mongoose';
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const emailTokenSchema = new Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    token: {
-        type: String,
-        required: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-        index: {expires: 1000*30}
-    }
-})
+  email: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['forgot', 'verify'],
+    required: true
+  },
+  token: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    index: { expires: 1000 * 30 }
+  }
+});
 
 export default mongoose.model('EmailToken', emailTokenSchema);
